d6bd6a2a746fa646c3ac9fe4d5088d0bebba98f2
Full-Stack-David <davidchan12323@gmail.com>
2022-07-22 03:22:27 -0500

    Angular sample for slider login

---
+++ README.md
@@ -0,0 +1,21 @@
+# Angular simple project for slider login
+
+## Install
+
+```sh
+$ npm install
+```
+
+## Develop
+
+```sh
+$ ng serve {project}
+```
+
+## Production
+
+```sh
+$ ng build {project}
+```
+
+## Deployment
---
+++ src/app/app.module.ts
@@ -0,0 +1,23 @@
+import '@angular/localize/init'
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
